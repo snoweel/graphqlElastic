d68be4dfa659d262e3c79e6f85e129c1632fbe87
Snoweel Pereira <Snoweel.Pereira@intellectdesign.com>
2017-11-24 13:42:31 +0530

    simple hardcoded working one

--- InquiryAPIs/schema.js
+++ InquiryAPIs/schema.js
@@ -2,32 +2,90 @@
  * declaring modules
  */
 const{
-    makeExecutableSchema
-  } = require('graphql-tools')
+  makeExecutableSchema
+} = require('graphql-tools')
 const graphql=require('graphql');
 const apollo=require('apollo-server-express')
 const userSchema = require('./users/Model/Schema/index')
 const todoSchema = require('./todo//Model/Schema/index')
-const resolver = require('./users/Model/Resolver/resolver')
+// const resolver = require('./users/Model/Resolver/resolver')
 
-const RootQuery = `
-type RootQuery {
-  getUserDetails(userId: String!): User,
-  getActiveUsers: [User]
+// console.dir(resolver);
+const typeDefs = `
+type User{
+userId:String
+fname:String
+lname:String
+email:String
+isActive:Boolean
+birthDate:String
+pinCode:Int
+todos:[ToDo]
 }
-`;
+type ToDo{
+id:String
+text:String
+userId:String
+done:Boolean
+targetDate:String
+}
 
-const SchemaDefinition = `
+type Query{
+
+#defining find user query
+getUserDetails(userId:String): User
+
+# defining find active users query
+getActiveUsers : [User]
+
+}
+
 schema {
-  query: RootQuery
+query: Query
 }
+
+
 `;
+
+const userData = [
+{
+  userId: "1",
+  id: "1",
+  fname: "Alex",
+  lname: "Galllio",
+  email: "dfdgf@hhj.com",
+  isActive: true,
+  birthDate: "12/12/1990",
+  pinCode: 12133
+}
+];
+
+const toDoData = [
+{
+  id: "String",
+  text: "String",
+  userId: "String",
+  done: false,
+  targetDate: "25-Nov-2017"
+}
+];
+/**
+* defining resolver functions
+*/
+const resolvers = {
+Query: {
+  getUserDetails: () => userData[0],
+  getActiveUsers: () => userData
+},
+User: {
+  todos: () => toDoData
+}
+};
 
 // const typeDef={userSchema,todoSchema}
 const schema = makeExecutableSchema(
-  { typeDefs:[SchemaDefinition, RootQuery,userSchema],
-    resolvers:resolver});
+{ typeDefs,resolvers});
 /**
- * exporting schema
- */
+* exporting schema
+*/
 module.exports=schema
